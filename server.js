e6d0fe122324a41d640491df4e671d52a431e28f
Ciaran <ciarangrimshawdev@gmail.com>
2020-06-15 12:55:59 +0100

    Initialise Sequelize

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the application" });
@@ -26,3 +34,21 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+// initial roles
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
